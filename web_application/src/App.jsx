5a5c6f2aa3689a005578ad69a1b26e940c3652c2
Nghi <nmnghi0109@gmail.com>
2025-06-16 09:09:23 +0700

    add landing page

--- web_application/src/App.jsx
+++ web_application/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import Window from './components/Window/Window'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <Sidebar/>
-      <Window/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/chat" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
